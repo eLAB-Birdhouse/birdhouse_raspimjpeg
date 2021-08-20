e2289d902c515016449f759a1ca1a76593470847
Vincent <vincent.stragier97@gmail.com>
2021-08-20 21:15:48 +0200

    Try to build the binaries.

---
+++ .github/workflows/setup_image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check for sudo permissions
+if [ "$EUID" != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+# Update and upgrade packages
+sudo apt-get update
+sudo apt-get full-upgrade -y
+
+sudo apt-get install -y git cmake
+
+# Enable camera after next reboot (optional) [not possible with chroot]
+# sudo raspi-config nonint do_camera 0 && sudo reboot
+
+# Clone raspimjpeg code
+git clone https://github.com/roberttidey/userland.git
+
+# Compile project
+cd userland
+sudo chmod +x ./buildme
+./buildme
+
+# cd bin
